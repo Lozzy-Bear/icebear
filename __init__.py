0afc1acb7ea79625d033dadcb81a442121a7f49e
Adam Lozinsky <arl203@mail.usask.ca>
2020-07-08 15:59:55 -0600

    trying to fix napoleon

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .src import imaging as imaging
-from .src import plotting as plotting
-from .src import processing as processing
-from .src import utils as utils
+import .src.imaging as imaging
+import .src.plotting as plotting
+import .src.processing as processing
+import .src.utils as utils
