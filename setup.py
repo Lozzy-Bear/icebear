1dc1d4289323fb7f2e263c72303260ac0e294e99
Devin-Huyghebaert <devin.huyghebaert@gmail.com>
2020-12-17 11:19:32 -0600

    added setup.py file for easy installation using pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(name='icebear',
+      version='0.0',
+      description='Utilities for processing ICEBEAR data',
+      author='Adam Lozinsky, Devin Huyghebaert, Draven Galeschuk',
+      author_email='devin.huyghebaert@gmail.com',
+      url='https://github.com/Lozzy-Bear/icebear',
+      packages=['icebear'],
+     )
