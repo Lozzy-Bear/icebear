1f054796f796d5b99121625acb9db2377ff8a935
Adam Lozinsky <arl203@mail.usask.ca>
2021-01-13 15:51:35 -0600

    fixing rtd

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import re
 sys.path.insert(0, os.path.abspath('../'))
 
 
@@ -22,7 +23,13 @@ copyright = '2020, Adam Lozinsky'
 author = 'Adam Lozinsky'
 
 # The full version, including alpha/beta/rc tags
-release = '0'
+# Get version number from __init__.py
+regex = "(?<=__version__..\s)\S+"
+with open(os.path.join(here, '../icebear/__init__.py'), 'r', encoding='utf-8') as f:
+    text = f.read()
+match = re.findall(regex, text)
+release = match[0].strip("'")
+version = release
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,7 +40,13 @@ needs_sphinx = '3.1.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
 # Napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
@@ -61,7 +74,18 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'#''sphinx_rtd_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    'github_user': 'Lozzy-Bear',
+    'github_repo': 'icebear',
+    'description': 'A Python package for processing, imaging, and plotting '
+                   'Ionospheric Continuous-wave E region Bistatic Experimental Auroral Radar (ICEBEAR) data.',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
