0f48fb0fb2bb1de324e389e40afb0b0a6fa6f719
Adam Lozinsky <arl203@mail.usask.ca>
2021-01-13 15:58:57 -0600

    fixing rtd

--- setup.py
+++ setup.py
@@ -100,8 +100,7 @@ if __name__ == "__main__":
             # installed, specify them here.  If using Python 2.6 or less, then these
             # have to be included in MANIFEST.in as well.
 
-            # package_data={'flipchem': ['dat/*'],
-                          },
+            # package_data={'icebear': ['dat/*'],},
             # include_package_data=True,
 
             # Although 'package_data' is the preferred approach, in some case you may
