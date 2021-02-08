bbcd0a189971324cb5e6832868cbe69f998a7e50
Adam Lozinsky <arl203@mail.usask.ca>
2021-02-08 03:37:37 -0600

    fit corr

---
+++ etc/scripts/geminids_correction.py
@@ -0,0 +1,124 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import icebear.utils as utils
+import icebear
+from scipy.optimize import curve_fit
+
+filepath = 'E:/icebear/level2a/'  # Enter file path to level 1 directory
+files = utils.get_all_data_files(filepath, '2020_12_12', '2020_12_15')  # Enter first sub directory and last
+el = np.array([])
+rng = np.array([])
+dop = np.array([])
+snr = np.array([])
+az = np.array([])
+
+for file in files:
+    f = h5py.File(file, 'r')
+    print(file)
+    group = f['data']
+    keys = group.keys()
+
+    for key in keys:
+        if f'{key}' == '052138000':
+            break
+        data = group[f'{key}']
+        rf_distance = data['rf_distance'][()]
+        snr_db = data['snr_db'][()]
+        doppler_shift = data['doppler_shift'][()]
+        azimuth = data['azimuth'][()]
+        elevation = data['elevation'][()]
+        #elevation_spread = data['elevation_extent'][()]
+        #azimuth_spread = data['azimuth_extent'][()]
+        area = data['area'][()]
+
+        rng = np.append(rng, rf_distance)
+        el = np.append(el, elevation)
+        dop = np.append(dop, doppler_shift)
+        snr = np.append(snr, snr_db)
+        az = np.append(az, azimuth)
+
+snr = np.abs(snr)
+az = np.abs(az)
+el += 90
+el = np.abs(el)
+rng = rng * 0.75 - 200
+m = np.ones_like(rng)
+m = np.ma.masked_where(dop > 20, m)
+m = np.ma.masked_where(dop < -20, m)
+m = np.ma.masked_where(snr <= 6.0, m)
+m = np.ma.masked_where(az >= 315, m)
+m = np.ma.masked_where(az <= 225, m)
+m = np.ma.masked_where(el >= 30, m)
+m = np.ma.masked_where(el <= 1, m)
+m = np.ma.masked_where(rng <= 300, m)
+m = np.ma.masked_where(rng >= 1200, m)
+
+re = 6378.0
+a = 6378.1370
+b = 6356.7523
+p1 = np.deg2rad(52.1579)
+r1 = np.sqrt((a*np.cos(p1))**2 + (b*np.sin(p1))**2)
+
+
+pre_alt = np.sqrt(re ** 2 + rng ** 2 - 2 * re * rng * np.cos(np.deg2rad(90 + np.abs(el))))
+gamma = np.rad2deg(np.arccos((rng ** 2 - (re ** 2) - (pre_alt ** 2)) / (-2 * re * pre_alt)))
+p2 = p1 + gamma
+r2 = np.sqrt((a*np.cos(p2))**2 + (b*np.sin(p2))**2)
+
+alt_geocentric = -re + np.sqrt(re ** 2 + rng ** 2 + 2 * re * rng * np.sin(np.deg2rad(el - gamma)))
+alt_normal = -re + np.sqrt(re**2 + rng**2 + 2 * re * rng * np.sin(np.deg2rad(el)))
+alt_negative = -re + np.sqrt(re**2 + rng**2 - 2 * re * rng * np.sin(np.deg2rad(el)))
+
+rng = rng * m
+dop = dop * m
+snr = snr * m
+az = az * m
+el = el * m
+alt_geocentric = alt_geocentric * m
+alt_normal = alt_normal * m
+alt_negative = alt_negative * m
+
+hist, _, _ = np.histogram2d(rng, alt_normal, bins=[np.arange(300, 1200, 1), np.arange(0, 400, 1)])
+h = np.copy(hist)
+h[h == 0] = np.nan
+mean_hs = np.zeros(h.shape[0])
+for i in range(len(mean_hs)):
+    mean_hs[i] = np.argmax(hist[i, :])
+
+xdata = np.arange(300, 1199, 1)
+xdata = np.ma.masked_where(mean_hs < 10, xdata)
+ydata = np.ma.masked_where(mean_hs < 10, mean_hs)
+xdata = np.ma.compressed(xdata)
+ydata = np.ma.compressed(ydata)
+
+
+def func(x, a, b, c):
+    return a * x**2 + b * x + c
+
+
+popt, pcov = curve_fit(func, xdata, ydata)
+print(popt)
+plt.figure()
+plt.title('Geminids Meteor Peak Occurrence Altitude vs. Range Bin')
+plt.scatter(xdata, ydata)
+plt.plot(xdata, func(xdata, *popt), 'r-', label='fit: a=%5.6f, b=%5.6f, c=%5.6f' % tuple(popt))
+plt.xlabel('Range [km]')
+plt.ylabel('Altitude [km]')
+plt.legend()
+
+plt.figure()
+plt.subplot(211)
+plt.scatter(rng, np.abs(alt_geocentric + func(rng, *popt) - 101), marker='o')
+plt.scatter(rng, alt_normal, marker='x')
+plt.legend(('2nd Order correction - Geocentric correction', 'Normal'))
+plt.xlabel('Range [km]')
+plt.ylabel('Altitude [km]')
+
+plt.subplot(212)
+plt.scatter(rng, np.abs(alt_normal - func(rng, *popt) + 101), marker='o')
+plt.scatter(rng, alt_geocentric, marker='x')
+plt.legend(('2nd Order correction', 'Geocentric correction'))
+plt.xlabel('Range [km]')
+plt.ylabel('Altitude [km]')
+plt.show()
--- etc/scripts/rng_el_plots.py
+++ etc/scripts/rng_el_plots.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import icebear.utils as utils
 import icebear
 
+
 filepath = 'E:/icebear/level2a/'  # Enter file path to level 1 directory
 files = utils.get_all_data_files(filepath, '2020_12_13', '2020_12_13')  # Enter first sub directory and last
 #files = ['E:/icebear/level2/2019_12_19/ib3d_normal_swht_01deg_2019_12_19_05_prelate_bakker.h5']  # Alternate; make a list of files with file paths.
@@ -39,55 +40,96 @@ for file in files:
         snr = np.append(snr, snr_db)
         az = np.append(az, azimuth)
 
-# az -= 270
-#el += 90
-#el = np.abs(el)
-
-hu = 0.015
+az = np.abs(az)
+el += 90
+el = np.abs(el)
 rng = rng * 0.75 - 200
-alt = -6378 + np.sqrt((6378 + hu) ** 2 + rng ** 2 + 2 * (6378 + hu) * rng * np.sin(np.deg2rad(el)))
-alt_fixed = -6378 + np.sqrt((6378 + hu) ** 2 + rng ** 2 - 2 * (6378 + hu) * rng * np.sin(np.deg2rad(el)))
-#alt_old = -6378 + np.sqrt((6378 + hu) ** 2 + rng ** 2 - 2 * (6378 + hu) * rng * np.cos(np.deg2rad(90 + el)))
+a = 6378.1370
+b = 6356.7523
+p1 = np.deg2rad(52.1579)
+r1 = np.sqrt((a*np.cos(p1))**2 + (b*np.sin(p1))**2)
 
+pre_alt = np.sqrt(r1 ** 2 + rng ** 2 - 2 * r1 * rng * np.cos(np.deg2rad(90 + np.abs(el))))
+gamma = np.arccos((rng ** 2 - (r1 ** 2) - (pre_alt ** 2)) / (-2 * r1 * pre_alt))
+el2 = el - np.abs(np.rad2deg(gamma))
+
+p2 = p1 + gamma
+r2 = np.sqrt((a*np.cos(p2))**2 + (b*np.sin(p2))**2)
+
+alt_geocentric = -r2 + np.sqrt(r1 ** 2 + rng ** 2 + 2 * r1 * rng * np.sin(np.deg2rad(el2)))
+#alt_negative = -r2 + np.sqrt(r1 ** 2 + rng ** 2 + 2 * r1 * rng * np.sin(np.deg2rad(-el)))
+alt_negative = -r1 + np.sqrt(r1 ** 2 + rng ** 2 + 2 * r1 * rng * np.sin(np.deg2rad(el2)))
+#alt_negative *= 1.017
 
 d = 20
 s = 6
 
 rng = np.where(dop <= d, rng, np.nan)
-alt = np.where(dop <= d, alt, np.nan)
-alt_fixed = np.where(dop <= d, alt_fixed, np.nan)
+pre_alt = np.where( dop <= d, pre_alt, np.nan)
+alt_geocentric = np.where(dop <= d, alt_geocentric, np.nan)
+alt_negative = np.where(dop <= d, alt_negative, np.nan)
 dop = np.where(dop <= d, dop, np.nan)
 az = np.where(dop <= d, az, np.nan)
 el = np.where(dop <= d, el, np.nan)
 
-rng = np.where(dop >= -d, rng, np.nan)
-alt = np.where(dop >= -d, alt, np.nan)
-alt_fixed = np.where(dop >= -d, alt_fixed, np.nan)
-dop = np.where(dop >= -d, dop, np.nan)
-az = np.where(dop >= -d, az, np.nan)
-el = np.where(dop >= -d, el, np.nan)
+rng = np.where(-d <= dop, rng, np.nan)
+pre_alt = np.where(-d <= dop, pre_alt, np.nan)
+alt_geocentric = np.where(-d <= dop, alt_geocentric, np.nan)
+alt_negative = np.where(-d <= dop, alt_negative, np.nan)
+dop = np.where(-d <= dop, dop, np.nan)
+az = np.where(-d <= dop, az, np.nan)
+el = np.where(-d <= dop, el, np.nan)
 
 rng = np.where(snr >= s, rng, np.nan)
-alt = np.where(snr >= s, alt, np.nan)
-alt_fixed = np.where(snr >= s, alt_fixed, np.nan)
+pre_alt = np.where(snr >= s, pre_alt, np.nan)
+alt_geocentric = np.where(snr >= s, alt_geocentric, np.nan)
+alt_negative = np.where(snr >= s, alt_negative, np.nan)
 dop = np.where(snr >= s, dop, np.nan)
 az = np.where(snr >= s, az, np.nan)
 el = np.where(snr >= s, el, np.nan)
 
-rng = np.where(az >= -180, np.nan, rng)
-alt = np.where(az >= -180, np.nan, alt)
-alt_fixed = np.where(az >= -180, np.nan, alt_fixed)
-dop = np.where(az >= -180, np.nan, dop)
-az = np.where(az >= -180, np.nan, az)
-el = np.where(az >= -180, np.nan, el)
+rng = np.where(az >= 315, np.nan, rng)
+pre_alt = np.where(az >= 315, np.nan, pre_alt)
+alt_geocentric = np.where(az >= 315, np.nan, alt_geocentric)
+alt_negative = np.where(az >= 315, np.nan, alt_negative)
+dop = np.where(az >= 315, np.nan, dop)
+az = np.where(az >= 315, np.nan, az)
+el = np.where(az >= 315, np.nan, el)
 
-rng = np.where(az <= -320, np.nan, rng)
-alt = np.where(az <= -320, np.nan, alt)
-alt_fixed = np.where(az <= -320, np.nan, alt_fixed)
-dop = np.where(az <= -320, np.nan, dop)
-az = np.where(az <= -320, np.nan, az)
-el = np.where(az <= -320, np.nan, el)
+rng = np.where(az <= 225, np.nan, rng)
+pre_alt = np.where(az <= 225, np.nan, pre_alt)
+alt_geocentric = np.where(az <= 225, np.nan, alt_geocentric)
+alt_negative = np.where(az <= 225, np.nan, alt_negative)
+dop = np.where(az <= 225, np.nan, dop)
+az = np.where(az <= 225, np.nan, az)
+el = np.where(az <= 225, np.nan, el)
 
+plt.figure()
+plt.subplot(311)
+plt.scatter(rng, np.abs(alt_negative - alt_geocentric), c=dop)
+plt.title('Difference between methods')
+plt.colorbar(label='Doppler [Hz]')
+#plt.scatter(rng, pre_alt-6378, c=dop)
+#plt.title('Uncorrected data')
+#plt.plot([0, 1300], [90, 90], 'k')
+#plt.plot([0, 1300], [130, 130], 'k')
+
+plt.subplot(312)
+plt.scatter(rng, alt_geocentric, c=dop)
+plt.colorbar(label='Doppler [Hz]')
+plt.title('Elliprical Earth -- Geocentric angle correction')
+plt.plot([0, 1300], [90, 90], 'k')
+plt.plot([0, 1300], [130, 130], 'k')
+
+plt.subplot(313)
+plt.scatter(rng, alt_negative, c=dop)
+plt.colorbar(label='Doppler [Hz]')
+plt.title('Circular Earth -- Geocentric angle correction')
+#plt.title('Negative angle correction')
+plt.plot([0, 1300], [90, 90], 'k')
+plt.plot([0, 1300], [130, 130], 'k')
+
+"""
 plt.figure(3)
 plt.subplot(121)
 plt.scatter(rng, el, c=az)
@@ -146,5 +188,6 @@ plt.scatter(rng, -alt_fixed, c=dop)
 plt.colorbar(label='Doppler [Hz]')
 plt.xlabel('Range [km]')
 plt.ylabel('Altitude [km]')
+"""
 
 plt.show()
